835465fc517d68547e7051f110dbfd76b87aa0a0
Sebastian Bernal <sebasb718@gmail.com>
2023-02-23 23:49:00 -0500

    Creating a sqs publisher with .Net

---
+++ SQS/SqsWith.Net/SqsPublisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
+using System.Text.Json;
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "juansbc@gmail.com",
+    FullName = "Juan Bernal",
+    DateOfBirth = new DateTime(1993, 1, 1),
+    GitHubUsername = "sebasb718"
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    }
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine();
