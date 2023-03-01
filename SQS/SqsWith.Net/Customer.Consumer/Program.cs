91b9556d16e501c7a0470304c9b95bcddc93e172
Sebastian Bernal <sebasb718@gmail.com>
2023-02-28 22:27:48 -0500

    First version of a .net sqs consumer app

---
+++ SQS/SqsWith.Net/Customer.Consumer/Program.cs
@@ -0,0 +1,13 @@
+using Amazon.SQS;
+using Customer.Consumer;
+using Customers.Consumer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<QueueSettings>(builder.Configuration.GetSection(QueueSettings.Key));
+builder.Services.AddSingleton<IAmazonSQS, AmazonSQSClient>();
+builder.Services.AddHostedService<QueueConsumerServices>();
+
+var app = builder.Build();
+
+app.Run();
---
+++ SQS/SqsWith.Net/Customer.Consumer/QueueConsumerServices.cs
@@ -0,0 +1,55 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using Customers.Consumer;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
+
+namespace Customer.Consumer
+{
+    public class QueueConsumerServices : BackgroundService
+    {
+        private readonly IAmazonSQS _sqs;
+        private readonly IOptions<QueueSettings> _queueSettings;
+        public QueueConsumerServices(IAmazonSQS sqs, IOptions<QueueSettings> queueSettings)
+        {
+            _sqs = sqs;
+            _queueSettings = queueSettings;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var queueUrlResponse = await _sqs.GetQueueUrlAsync("customers", stoppingToken);
+
+            var receiveMessageRequest = new ReceiveMessageRequest
+            {
+                QueueUrl = queueUrlResponse.QueueUrl,
+                AttributeNames = new List<string>() { "All" },
+                MessageAttributeNames = new List<string>() { "All" },
+                MaxNumberOfMessages = 1
+            };
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var response = await _sqs.ReceiveMessageAsync(receiveMessageRequest, stoppingToken);
+                foreach (var message in response.Messages)
+                {
+                    var messageType = message.MessageAttributes["MessageType"].StringValue;
+
+                    switch (messageType)
+                    {
+                        case nameof(CustomerCreated):
+                            var created = JsonSerializer.Deserialize<CustomerCreated>(message.Body);
+                            break;
+                        case nameof(CustomerUpdated):
+                            break;
+                        case nameof(CustomerDeleted):
+                            break;
+                    }
+
+                    await _sqs.DeleteMessageAsync(queueUrlResponse.QueueUrl, message.ReceiptHandle, stoppingToken);
+                }
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
