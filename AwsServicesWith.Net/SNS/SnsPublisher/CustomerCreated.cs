ba86ec3965c0f5f576aabd8beb0a28ed9b206954
Juan Sebastian Bernal Cubides <j.bernal@globant.com>
2023-03-02 23:37:21 -0500

    Creating a sns publisher

---
+++ AwsServicesWith.Net/SNS/SnsPublisher/CustomerCreated.cs
@@ -0,0 +1,11 @@
+﻿namespace SnsPublisher
+{
+    public class CustomerCreated
+    {
+        public required Guid Id { get; init; }
+        public required string FullName { get; init; }
+        public required string Email { get; init; }
+        public required string GitHubUsername { get; init; }
+        public required DateTime DateOfBirth { get; init; }
+    }
+}
---
+++ AwsServicesWith.Net/SNS/SnsPublisher/Program.cs
@@ -0,0 +1,35 @@
+﻿using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using SnsPublisher;
+using System.Text.Json;
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "juansbc@gmail.com",
+    FullName = "Juan Bernal",
+    DateOfBirth = new DateTime(1993, 1, 1),
+    GitHubUsername = "sebasb718"
+};
+
+var snsClient = new AmazonSimpleNotificationServiceClient();
+
+var topicArnResponse = await snsClient.FindTopicAsync("customers");
+
+var publisherRequest = new PublishRequest
+{
+    TopicArn = topicArnResponse.TopicArn,
+    Message = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    }
+};
+
+var response = await snsClient.PublishAsync(publisherRequest);
