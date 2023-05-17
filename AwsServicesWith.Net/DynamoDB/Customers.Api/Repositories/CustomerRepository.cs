12a084dd1657029bf5a3f036c4fe19448d83fec4
Juan Sebastian Bernal Cubides <j.bernal@globant.com>
2023-05-17 00:11:29 -0500

    Implementing the CRUD operation in DynamoDB (not including the read all)

--- AwsServicesWith.Net/DynamoDB/Customers.Api/Repositories/CustomerRepository.cs
+++ AwsServicesWith.Net/DynamoDB/Customers.Api/Repositories/CustomerRepository.cs
@@ -1,17 +1,57 @@
-﻿using Customers.Api.Contracts.Data;
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+using Customers.Api.Contracts.Data;
+using System.Text.Json;
 
 namespace Customers.Api.Repositories;
 
 public class CustomerRepository : ICustomerRepository
 {
+    private readonly IAmazonDynamoDB _dynamoDb;
+    private readonly string _tableName = "customers";
+
+    public CustomerRepository(IAmazonDynamoDB dynamoDb)
+    {
+        _dynamoDb = dynamoDb;
+    }
+
     public async Task<bool> CreateAsync(CustomerDto customer)
     {
-        throw new NotImplementedException();
+        customer.UpdatedAt = DateTime.UtcNow;
+        var customerAsJson = JsonSerializer.Serialize(customer);
+        var customerAsAttribute = Document.FromJson(customerAsJson).ToAttributeMap();
+
+        var createItemRequest = new PutItemRequest
+        {
+            TableName = _tableName,
+            Item = customerAsAttribute
+        };
+
+        var response = await _dynamoDb.PutItemAsync(createItemRequest);
+        return response.HttpStatusCode == System.Net.HttpStatusCode.OK;
     }
 
     public async Task<CustomerDto?> GetAsync(Guid id)
     {
-        throw new NotImplementedException();
+        var getItemRequest = new GetItemRequest
+        {
+            TableName = _tableName,
+            Key = new Dictionary<string, AttributeValue>
+            {
+                { "pk", new AttributeValue{ S = id.ToString() } },
+                { "sk", new AttributeValue{ S = id.ToString() } }
+            }
+        };
+
+        var response = await _dynamoDb.GetItemAsync(getItemRequest);
+        if (response.Item.Count == 0)
+        {
+            return null;
+        }
+
+        var itemAsDocument = Document.FromAttributeMap(response.Item);
+        return JsonSerializer.Deserialize<CustomerDto>(itemAsDocument.ToJson());
     }
 
     public async Task<IEnumerable<CustomerDto>> GetAllAsync()
@@ -21,11 +61,33 @@ public class CustomerRepository : ICustomerRepository
 
     public async Task<bool> UpdateAsync(CustomerDto customer)
     {
-        throw new NotImplementedException();
+        customer.UpdatedAt = DateTime.UtcNow;
+        var customerAsJson = JsonSerializer.Serialize(customer);
+        var customerAsAttribute = Document.FromJson(customerAsJson).ToAttributeMap();
+
+        var updateItemRequest = new PutItemRequest
+        {
+            TableName = _tableName,
+            Item = customerAsAttribute
+        };
+
+        var response = await _dynamoDb.PutItemAsync(updateItemRequest);
+        return response.HttpStatusCode == System.Net.HttpStatusCode.OK;
     }
 
     public async Task<bool> DeleteAsync(Guid id)
     {
-        throw new NotImplementedException();
+        var deletedItemRequest = new DeleteItemRequest
+        {
+            TableName = _tableName,
+            Key = new Dictionary<string, AttributeValue>
+            {
+                { "pk", new AttributeValue{ S = id.ToString() } },
+                { "sk", new AttributeValue{ S = id.ToString() } }
+            }
+        };
+
+        var response = await _dynamoDb.DeleteItemAsync(deletedItemRequest);
+        return response.HttpStatusCode == System.Net.HttpStatusCode.OK;
     }
 }
