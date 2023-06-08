5ec2339abf9bd99b81dc24282d9f7c252f78c132
Juan Sebastian Bernal Cubides <sebasb718@gmail.com>
2023-06-08 16:35:44 -0500

    Example of reading a file from S3 with C#

--- AwsServicesWith.Net/S3/S3Playground/Program.cs
+++ AwsServicesWith.Net/S3/S3Playground/Program.cs
@@ -1,17 +1,36 @@
 ﻿using Amazon.S3;
 using Amazon.S3.Model;
+using System.Text;
 
 var s3Client = new AmazonS3Client();
 
+//Code for loading files into S3
+/*
 await using var inputStream = new FileStream("./movies.csv", FileMode.Open, FileAccess.Read);
 
 var putObjectRequest = new PutObjectRequest
 {
     BucketName = "juanawscourse",
-    Key = "files/images.csv",
+    Key = "files/movies.csv",
     ContentType = "text/csv",
     InputStream = inputStream
 };
 
 await s3Client.PutObjectAsync(putObjectRequest);
+*/
 
+//Code for Reading files for S3
+var getObjectRequest = new GetObjectRequest
+{
+    BucketName = "juanawscourse",
+    Key = "files/movies.csv"
+};
+
+var response = await  s3Client.GetObjectAsync(getObjectRequest);
+
+using var memoryStream = new MemoryStream();
+response.ResponseStream.CopyTo(memoryStream);
+
+var text = Encoding.Default.GetString(memoryStream.ToArray());
+
+Console.WriteLine(text);
