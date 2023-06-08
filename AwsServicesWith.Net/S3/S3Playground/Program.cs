be3579a54c075501fc599e9a782a98d8ffbdd39e
Juan Sebastian Bernal Cubides <j.bernal@globant.com>
2023-06-08 16:14:11 -0500

    Example of loading a file into S3 with C#

--- AwsServicesWith.Net/S3/S3Playground/Program.cs
+++ AwsServicesWith.Net/S3/S3Playground/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine();
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+
+var s3Client = new AmazonS3Client();
+
+await using var inputStream = new FileStream("./movies.csv", FileMode.Open, FileAccess.Read);
+
+var putObjectRequest = new PutObjectRequest
+{
+    BucketName = "juanawscourse",
+    Key = "files/images.csv",
+    ContentType = "text/csv",
+    InputStream = inputStream
+};
+
+await s3Client.PutObjectAsync(putObjectRequest);
+
