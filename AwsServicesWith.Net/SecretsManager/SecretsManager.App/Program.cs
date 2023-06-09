89f56e8f1ad3d942bbed78a5866ae4e90220b2dc
Juan Sebastian Bernal Cubides <sebasb718@gmail.com>
2023-06-09 15:26:27 -0500

    Adding small console app to consume a secret using C#

---
+++ AwsServicesWith.Net/SecretsManager/SecretsManager.App/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+var secretsManagerClient = new AmazonSecretsManagerClient();
+
+var request = new GetSecretValueRequest
+{
+    SecretId = "ApiKey"
+};
+
+var response = await secretsManagerClient.GetSecretValueAsync(request);
+
+Console.Write(response.SecretString);
+
+var describeSecretRequest = new DescribeSecretRequest
+{
+    SecretId = "ApiKey"
+};
+
+var describeResponse = await secretsManagerClient.DescribeSecretAsync(describeSecretRequest);
+
+Console.WriteLine();
