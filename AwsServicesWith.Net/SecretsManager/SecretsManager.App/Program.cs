586530ad6b63ca6c00c334f08ee6f1c93e854bbf
Juan Sebastian Bernal Cubides <sebasb718@gmail.com>
2023-06-13 16:52:43 -0500

    Checking how to access old versions of secrets

--- AwsServicesWith.Net/SecretsManager/SecretsManager.App/Program.cs
+++ AwsServicesWith.Net/SecretsManager/SecretsManager.App/Program.cs
@@ -4,9 +4,18 @@ using Amazon.SecretsManager.Model;
 
 var secretsManagerClient = new AmazonSecretsManagerClient();
 
+var listSecretVersionsRequest = new ListSecretVersionIdsRequest
+{
+    SecretId = "ApiKey",
+    IncludeDeprecated = true
+};
+
+var versionResponse = await secretsManagerClient.ListSecretVersionIdsAsync(listSecretVersionsRequest);
+
 var request = new GetSecretValueRequest
 {
-    SecretId = "ApiKey"
+    SecretId = "ApiKey",
+    VersionId = "269ab378-d54d-4dfa-9c94-b037fc3991c1"
 };
 
 var response = await secretsManagerClient.GetSecretValueAsync(request);
