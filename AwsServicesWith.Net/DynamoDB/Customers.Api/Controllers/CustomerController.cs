fb545aedb965e2f44268a9fd3137d80022d7e4c7
Juan Sebastian Bernal Cubides <sebasb718@gmail.com>
2023-06-13 22:34:14 -0500

    Fixing routing issue while creating customer in Customers.API

--- AwsServicesWith.Net/DynamoDB/Customers.Api/Controllers/CustomerController.cs
+++ AwsServicesWith.Net/DynamoDB/Customers.Api/Controllers/CustomerController.cs
@@ -24,7 +24,7 @@ public class CustomerController : ControllerBase
 
         var customerResponse = customer.ToCustomerResponse();
 
-        return CreatedAtAction("Get", new { customerResponse.Id }, customerResponse);
+        return CreatedAtAction("Get", new { idOrEmail = customerResponse.Id }, customerResponse);
     }
 
     [HttpGet("customers/{idOrEmail}")]
