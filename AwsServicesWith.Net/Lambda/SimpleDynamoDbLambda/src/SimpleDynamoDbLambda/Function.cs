b4c87e16afeecafada13562c40eae7a686bf6d6b
Juan Sebastian Bernal Cubides <sebasb718@gmail.com>
2023-06-14 16:47:28 -0500

    Creating a lambda triggered by DynamoDB

---
+++ AwsServicesWith.Net/Lambda/SimpleDynamoDbLambda/src/SimpleDynamoDbLambda/Function.cs
@@ -0,0 +1,37 @@
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.DynamoDBEvents;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace SimpleDynamoDbLambda
+{
+    public class Function
+    {
+        public void FunctionHandler(DynamoDBEvent dynamoEvent, ILambdaContext context)
+        {
+            context.Logger.LogInformation($"Beginning to process {dynamoEvent.Records.Count} records...");
+
+            foreach (var record in dynamoEvent.Records)
+            {
+                context.Logger.LogInformation($"Event ID: {record.EventID}");
+                context.Logger.LogInformation($"Event Name: {record.EventName}");
+
+                if(record.Dynamodb.OldImage != null)
+                {
+                    var oldDocument = Document.FromAttributeMap(record.Dynamodb.OldImage);
+                    context.Logger.LogInformation($"Old Document: {oldDocument.ToJsonPretty()}");
+                }
+
+                var newDocument = Document.FromAttributeMap(record.Dynamodb.NewImage);
+                context.Logger.LogInformation($"New Document: {newDocument.ToJsonPretty()}");
+
+                // TODO: Add business logic processing the record.Dynamodb object.
+            }
+
+            context.Logger.LogInformation("Stream processing complete.");
+        }
+    }
+}
